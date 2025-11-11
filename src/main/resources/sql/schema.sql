7c31986ae0d3ac554cc452befb7984ab45e64ea8
JeonghoonAHN0510 <Jeonghoonahn0510@gmail.com>
2025-11-11 17:55:57 +0900

    build : ProcessExchange, Project 관련 Entity, Dto 생성

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -4,9 +4,9 @@ alter table unitgroup auto_increment = 30001;
 alter table units auto_increment = 40001;
 alter table flow auto_increment = 100001;
 alter table process_info auto_increment = 200001;
-
+alter table process_exchange auto_increment = 100000001;
+alter table project auto_increment = 300001;
 -- 추후 테이블 생성 시, 활성화하며 ALTER 추가
--- alter table process_exchange auto_increment = 100000001;
--- alter table project auto_increment = 300001;
+
 -- alter table project_exchange auto_increment = 200000001;
 -- alter table project_result auto_increment = 300000001;
