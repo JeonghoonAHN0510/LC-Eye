9b06974d21973535f628df18e42f190bd3052253
msh-94 <alstjdgh2003@naver.com>
2025-11-13 17:24:17 +0900

    feat : 투입물·산출물 저장 구현중

---
+++ src/main/java/lceye/service/ExchangeService.java
@@ -0,0 +1,16 @@
+package lceye.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Map;
+
+@Service @Transactional
+public class ExchangeService {
+
+    public boolean saveInfo(Map<String, Object> exchangeList){
+
+    }// func end
+
+}// class end
