0cf1540cd42faba3557e89df2fb89b4e6ec58a71
JeonghoonAHN0510 <Jeonghoonahn0510@gmail.com>
2025-11-13 17:26:40 +0900

    chore : 파비콘 및 로고 추가 - Header.jsx : 간단 CSS 추가

--- src/main/lceye/src/admin/ProjectRouter.jsx
+++ src/main/lceye/src/admin/ProjectRouter.jsx
@@ -1,15 +1,18 @@
 import Header from './components/layout/Header.jsx';
 import ProjectLeftSection from "./pages/project/ProjectLeftSection";
 import ProjectRightSection from "./pages/project/ProjectRightSection";
+import '../assets/css/header.css';
 
-export default function ProjectRouter(props){
-    return(
+export default function ProjectRouter(props) {
+    return (
         <>
-        <Header />
-        <div style={{display:"flex", justifyContent:"space-evenly"}}>
-            <ProjectLeftSection/>
-            <ProjectRightSection/>
-        </div>
+            <div className='header'>
+                <Header />
+            </div>
+            <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+                <ProjectLeftSection />
+                <ProjectRightSection />
+            </div>
         </>
     ) // return end
 } // func end
--- src/main/lceye/src/admin/components/layout/Header.jsx
+++ src/main/lceye/src/admin/components/layout/Header.jsx
@@ -1,7 +1,29 @@
-export default function Header(props){
-    return(
+import { useSelector } from 'react-redux';
+import { Navigate } from "react-router-dom";
+import headerLogo from '../../../assets/img/LC-Eye_HeaderLogo.svg';
+
+export default function Header(props) {
+    const { isLogin } = useSelector((state) => state.admin);
+    console.log(isLogin);
+
+    // 혹시 비로그인 상태인데 들어와졌으면, 메인페이지로 이동
+    if (isLogin.isAuth != true) return <Navigate to="/" />
+    return (
         <>
-        <h2> Header.jsx</h2>
+            <div className='imgBox'>
+                <img src={headerLogo} alt="headerLogo" />
+            </div>
+            <div className='infoBox'>
+                <span>{isLogin.mname}</span>님 <br />
+                소속 : {isLogin.cname} <br />
+
+                권한 : {
+                    isLogin.role == "ADMIN" ? "시스템 관리자"
+                        : isLogin.role == "MANAGER" ? "회사 관리자"
+                            : "실무자"
+                }
+
+            </div>
         </>
     ) // return end
 } // func end
