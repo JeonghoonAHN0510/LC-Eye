5931ce366d9569b5a9b6476b41a71dd2c37f76ad
JeonghoonAHN0510 <Jeonghoonahn0510@gmail.com>
2025-11-17 11:47:31 +0900

    feat : React 프로젝트 좌우분할

--- src/main/lceye/src/admin/ProjectRouter.jsx
+++ src/main/lceye/src/admin/ProjectRouter.jsx
@@ -2,6 +2,7 @@ import Header from './components/layout/Header.jsx';
 import ProjectLeftSection from "./pages/project/ProjectLeftSection";
 import ProjectRightSection from "./pages/project/ProjectRightSection";
 import '../assets/css/project.css';
+import SimpleSplitPane from './components/layout/SplitPaneResponsive.jsx';
 
 export default function ProjectRouter(props) {
     return (
@@ -9,14 +10,13 @@ export default function ProjectRouter(props) {
             <div className='header'>
                 <Header />
             </div>
-            <div className='projectSection'>
-                <div className='projectLeftSection'>
-                    <ProjectLeftSection />
-                </div>
-                <div className='projectRightSection'>
-                    <ProjectRightSection />
-                </div>
-            </div>
+                <SimpleSplitPane
+                    initLeftPct={50}              // 초기 좌측 폭(%)
+                    minLeftPx={240}               // 좌측 최소(px)
+                    minRightPx={320}              // 우측 최소(px)
+                    left={<ProjectLeftSection />}        // 좌측 콘텐츠
+                    right={<ProjectRightSection />}     // 우측 콘텐츠
+                />
         </>
     ) // return end
 } // func end
