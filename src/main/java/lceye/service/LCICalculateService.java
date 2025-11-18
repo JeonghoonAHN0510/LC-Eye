a020624b5b819b65e3c8ca356db4ac8bbdfbcb3f
OngTK <dhdxorud@gmail.com>
2025-11-18 17:55:23 +0900

    chore : 프로젝트 관련 front 구현시작

--- src/main/java/lceye/service/LCICalculateService.java
+++ src/main/java/lceye/service/LCICalculateService.java
@@ -6,12 +6,8 @@ import lceye.model.entity.ProjectResultFileEntity;
 import lceye.model.repository.ProjectRepository;
 import lceye.model.repository.ProjectResultFileRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
