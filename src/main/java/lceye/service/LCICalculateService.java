e09fffcc6cf4104f439d858a6d546e2d8549c1ef
OngTK <dhdxorud@gmail.com>
2025-11-23 13:52:15 +0900

    feat : AWS S3 CRUD 기능 구현 완료

--- src/main/java/lceye/service/LCICalculateService.java
+++ src/main/java/lceye/service/LCICalculateService.java
@@ -6,6 +6,7 @@ import lceye.model.entity.ProjectResultFileEntity;
 import lceye.model.repository.MemberRepository;
 import lceye.model.repository.ProjectRepository;
 import lceye.model.repository.ProjectResultFileRepository;
+import lceye.util.FileUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,7 +21,7 @@ import java.util.*;
 public class LCICalculateService {
 
     private final ProjectRepository projectRepository;
-    private final FileService fileService;
+    private final FileUtil fileUtil;
     private final ProjectResultFileRepository projectResultFileRepository;
     private final MemberRepository memberRepository;
 
@@ -32,7 +33,7 @@ public class LCICalculateService {
         ProjectEntity projectEntity = projectRepository.getReferenceById(pjno);
         String projectExchangeFileName = projectEntity.getPjfilename();
         // [2] projectExchangeFileName 의 json 파일을 찾음 + 읽음 // json 파일 읽힘 확인
-        Map<String, Object> readFileData = fileService.readFile("exchange", projectExchangeFileName);
+        Map<String, Object> readFileData = fileUtil.readFile("exchange", projectExchangeFileName);
         // [3] processExchanges 를 리스트로 꺼냄
         List<Map<String, Object>> processExchanges = (List<Map<String, Object>>) readFileData.get("exchanges");
         double pjamount = (double) readFileData.get("pjamount");
@@ -110,7 +111,7 @@ public class LCICalculateService {
         // [10] 파일명 만들기
         String resultFileName = makeResultFileName(projectEntity); // cno_pjno_result_yyyyMMdd_HHmm.json
         // [11] 파일 저장하기 ( type, 파일명 ,JSON으로 변환할 데이터)
-        fileService.writeFile("result", resultFileName, resultJson);
+        fileUtil.uploadFile("result", resultFileName, resultJson);
 
         // [12] resultfile 매핑 테이블 결과 저장
         ProjectResultFileEntity projectResultFileEntity = ProjectResultFileEntity.builder()
@@ -152,7 +153,7 @@ public class LCICalculateService {
         }
         // [2] 캐쉬에 존재하지 않는다면
         // puuid에 해당하는 프로세스 JSON 조회
-        Map<String, Object> process = fileService.searchProcessJson(puuid);
+        Map<String, Object> process = fileUtil.searchProcessJson(puuid);
         // 프로세스 캐쉬에 해댱 uuid와 프로세스 정보를 Map 형태로 저장
         processCache.put(puuid, process);
         // 해당 프로세스를 반환
@@ -346,7 +347,7 @@ public class LCICalculateService {
         String fileName = projectResultFileRepository.returnFilename(pjno);
         if (fileName == null) return null;
         // [2] 파일명으로 파일 찾아오기
-        Map<String, Object> file = fileService.readFile("result", fileName);
+        Map<String, Object> file = fileUtil.readFile("result", fileName);
         // [3] results 항목만 가져오기
         List<Map<String, Object>> results = (List<Map<String, Object>>) file.get("results");
         // [4] result에서 input과 output을 구별
